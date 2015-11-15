e225038e6924bdbae8df61ccc7f579902cfe1c88
piotrekb3 <pepeb333@gmail.com>
2015-11-15 20:13:26 +0100

    dodanie projektu

---
+++ Ogloszenia drobne/Ogloszenia drobne/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ogloszenia_drobne.MvcApplication" Language="C#" %>
---
+++ Ogloszenia drobne/Ogloszenia drobne/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ogloszenia_drobne.Startup))]
+namespace Ogloszenia_drobne
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
